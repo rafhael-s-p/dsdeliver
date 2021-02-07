f810d0585da9ef197a7afc49bc6435241d8f942d
Rafhael <rafhael.dev@gmail.com>
2021-02-07 16:04:24 -0300

    Finished front web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import { ToastContainer } from 'react-toastify';
 import './App.css';
 import Routes from './Routes';
 
 function App() {
   return (
-    <Routes/>
+		<>
+    	<Routes/>
+			<ToastContainer/>
+		</>
   );
 }
 
